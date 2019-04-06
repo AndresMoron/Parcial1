351a6ec93da4c64f1db3887517302c7e1d9da09a
Andres Moron <andresmoron622@gmail.com>
2019-04-06 09:33:06 -0400

    Commit MVC

---
+++ Lista Modelo Parcial/AndresMoronFriend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AndresMoronFriend.MvcApplication" Language="C#" %>
---
+++ Lista Modelo Parcial/AndresMoronFriend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AndresMoronFriend.Startup))]
+namespace AndresMoronFriend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
